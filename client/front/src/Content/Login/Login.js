3e666ce0b1761d9971155573a09d198297e517d2
aarya moktan <aaryamoktan2055@gmail.com>
2023-07-03 10:03:27 -0700

    commit first

--- client/front/src/Content/Login/Login.js
+++ client/front/src/Content/Login/Login.js
@@ -1,21 +1,42 @@
-import React from 'react'
-import Registration from './Registration'
+import React, { useState } from 'react'
+import axios from "axios"
 import { Link } from 'react-router-dom'
 const Login = () => {
+  const [data,setdata] = useState({
+    name:"",
+    password:''
+  })
+  const inputhandle=(e)=>
+  {
+    setdata({...data,[e.target.name]:e.target.value})
+  }
+  const submit = (e)=>
+  {
+    e.preventDefault();
+    axios.push("/login",{data}).then(()=>
+    {
+      console.log("welcomw to my world")
+    })
+    .catch((err)=>
+    {
+      console.log("hellow error")
+    })
+  }
+ 
   return (
     <>
       <div className='logincontainer'>
       <img src={require("../images/background.jpg")}/>
         <div className='loginform'>
-          <form className='form'>
+          <form className='form' onSubmit={submit}>
             <div className='input'>
               <label>User Id</label>
               <br/>
-              <input type="text"  name="name"/>
+              <input type="text"  name="name" onChange={inputhandle} required/>
               <br/>
               <label>Password</label>
               <br/>
-              <input type="password" name="password"/>
+              <input type="password" name="password" onChange={inputhandle} required/>
               <br/>
               <button type="submit">Login</button>
             </div>
--- client/front/src/Content/Login/Registration.js
+++ client/front/src/Content/Login/Registration.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 const Registration = () => {
   const Navigate = useNavigate();
-  const submit =()=>
+  const [data,setdata] =useState(
+    {
+      firstname:'',
+      lastname:'',
+      select:'',
+      date:'',
+      country:'',
+      number:'',
+      email:''
+    }
+  );
+  const inputhandle = (e)=>
   {
-    alert("registrtion sucessfull")
-    Navigate("/")
+    setdata({...data,[e.target.name]:e.target.value})
   }
+  const submit =(e)=>
+  {
+    e.preventDefault();
+    axios.post("/registration",{data}).then
+    (()=>
+    {
+      console.log(data)
+      alert("registrtion sucessfull")
+    Navigate("/") 
+    })
+    .catch((err)=>
+    {
+      console.log(err)
+    })
+   
+  }
+  
   return (
     <>
       <div className='registrationContainer'>
@@ -15,15 +43,15 @@ const Registration = () => {
           <form onSubmit={submit}>
             <label>First-Name:</label>
             <br />
-            <input type="text" name="firstname" required></input>
+            <input type="text" name="firstname" onChange={inputhandle} required></input>
             <br />
             <label>Last-Name:</label>
             <br />
-            <input type="text" name="lastname" required></input>
+            <input type="text" name="lastname" required onChange={inputhandle}></input>
             <br />
             <label>Gender:</label>
             <br/>
-            <select name="select" required>
+            <select name="select" onChange={inputhandle} required>
               <option value="Male">Male</option>
               <option value="Female">Female</option>
               <option value="Others">Others</option>
@@ -31,19 +59,19 @@ const Registration = () => {
             <br />
             <label>Date_of_birth:</label>
             <br />
-            <input type="date" name="date" required></input>
+            <input type="date" name="date" onChange={inputhandle} required></input>
             <br />
             <label>Birth_Country:</label>
             <br />
-            <input type="text" name="country" required></input>
+            <input type="text" name="country" onChange={inputhandle} required></input>
             <br />
             <label>Number:</label>
             <br />
-            <input type="Number" name="number" required></input>
+            <input type="Number" name="number" onChange={inputhandle} required></input>
             <br />
             <label>Email_id:</label>
             <br />
-            <input type="email" name="email" required></input>
+            <input type="email" name="email" onChange={inputhandle} required></input>
             <br/>
             <button type="submit">Registration</button>
           </form>
